43ca72a52f18a312b798485c483feac9b9d43dbe
yousra Badji <37744104+yousra33@users.noreply.github.com>
2018-04-04 20:37:36 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,27 @@
 #!/bin/bash
+#Déplacement des fichiers sources du programme initdev partie 5.1
+#creation du fichier caché .initdev
+if [ -d "$HOME/.initdev" ]; then
+	exit 1;
+else
+#copier les dossiers qui sont dans le repertoire initdev dans le fichier caché .initdev
+	mkdir $HOME/.initdev;
+	cp ./initdev $HOME/.initdev/;
+	cp -r ./sources $HOME/.initdev/sources;
+	cp -r ./gitignores $HOME/.initdev/gitignores;
+	cp -r ./licenses $HOME/.initdev/licenses;
+	cp -r ./bin $HOME/.initdev/bin;
+	cp -r ./makefiles $HOME/.initdev/makefiles;
+	exit 0;
+
+fi
+
+
+		
+   			PATH="$PATH:/home/"$USER"/.initdev/"
+			export PATH 
+		
+		
+
+	echo "application installée"
+fi
