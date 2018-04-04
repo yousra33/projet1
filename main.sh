730881e6cd4544dc2445b1e824dd86c7d570ec40
yousra Badji <37744104+yousra33@users.noreply.github.com>
2018-04-04 23:39:52 +0100

    Update main.sh

--- main.sh
+++ main.sh
@@ -8,7 +8,7 @@ then
     if [ $# = 1] then
     echo"  Welcom to initdev's help" 
     echo "  
-            -NAME:.initdev/  le but de cette application indev est d'implémenter un outil d’initialisation de projets.
+            -NAME:.initdev/  le but de cette application est d'implémenter un outil d’initialisation de projets.
     
             -SYNTAX:-syntax1 :/initdev <nom-project/type-de-langage > #indiquer le nom et le langage
             
@@ -30,69 +30,85 @@ then
                     
                     -GPL:GENERAL PUBLIC LICENCE
                     
-                    -MIT:MIT LICENCE   
+                    -MIT:MIT LICENCE 
+		    
             AUTHOR: BADJI YOUSRA  email: yousrabaji33@gmail.com
+	    
             fi
-            pwd 
+	    
+            $pwd 
             cd .initdev
             read $1
            if [ "$1" = "-GPL" ] then
-								cd "$1"/
-								git init
-								cd ../
-								touch "$1"/LICENSEs
+	        cd "$1"/             
+		#redirection du fichier vers le repertoire parent LICENCES
+		cd ../
+		git init
+		touch "$1"/LICENSES
                 mkdir -p $1/{LICENCES
-								touch "$1"/makefile
+		touch "$1"/makefile
 					 
                 if [ "$1" = "-c" ] then
-								cd "$1"/
-								git init
-								cd ../
-								touch "$1"/gitifnores
+          	cd "$1"/
+	       git init
+	       cd ../
+	       #redirection du fichier vers le repertoire parent gitignores 
+		touch "$1"/gitifnores
                 mkdir -p $1/{gitignores}
-								touch "$1"/makefile
-								fi
+		touch "$1"/makefile
+		fi
+		
                  if [ "$1" = "-cpp" ] then
-								cd "$1"/
-								git init
-								cd ../
-								touch "$1"/gitifnores
+		cd "$1"/
+		git init                   
+		cd ../
+             #redirection du fichier vers le repertoire parent gitignores
+		touch "$1"/gitifnores
                 mkdir -p $1/{gitignores}
-								touch "$1"/makefile
-								fi
+		touch "$1"/makefile
+		fi
+		
                  if [ "$1" = "-py" ] then
-								cd "$1"/
-								git init
-								cd ../
-								touch "$1"/gitifnores
+		cd "$1"/          
+		git init
+		cd ../
+		#redirection du fichier vers le repertoire parent gitgnores
+		touch "$1"/gitifnores
                 mkdir -p $1/{gitignores}
-								touch "$1"/makefile
-								fi
+		touch "$1"/makefile
+		fi
+		
                  if [ "$1" = "latex" ] then
-								cd "$1"/
-								git init
-								cd ../
-								touch "$1"/gitifnores
+		cd "$1"/
+		git init
+		cd ../
+		#redirection du fichier vers le repertoire parent gitgnores
+		touch "$1"/gitifnores
                 mkdir -p $1/{gitignores}
-								touch "$1"/makefile
-								fi
+		touch "$1"/makefile
+		fi
+		
                  if [ "$1" = "BEAMER" ] then
-								cd "$1"/
-								git init
-								cd ../
-								touch "$1"/gitignores
+		cd "$1"/
+		ion du fichier vers le repertoire parent gitgnores
+		cd ../
+		git init
+		#redirect
+		touch "$1"/gitignores
                 mkdir -p $1/{gitignores}
-								touch "$1"/makefile
-								fi
+		touch "$1"/makefile
+		fi
            
             if [ "$1" = "-MIT" ] then
-								cd "$1"/
-								git init
-								cd ../
-								touch "$1"/LICENSES
+		cd "$1"/
+		git init
+	        cd ../
+		#redirection du fichier vers le repertoire parent LICENCES
+		touch "$1"/LICENSES
                 mkdir -p $1/{LICENCES}
-								touch "$1"/makefile
+		touch "$1"/makefile
                 fi
+		
                 touch main.sh
                 touch install.sh
                 exit 1
